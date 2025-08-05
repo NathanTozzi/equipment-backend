3beb0eaa306614ac467d81e9c0b9eaab9ea2b6f4
Nathan Tozzi <nathantozzi@Nathans-MacBook-Pro-2.local>
2025-08-04 22:15:48 -0400

    Initial backend upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route('/api/search')
+def search_equipment():
+    query = request.args.get('query')
+    specs = {
+        'model': query,
+        'weight': '6,000 lbs',
+        'engine': 'Kubota Diesel V2403',
+        'operating_capacity': '2,000 lbs'
+    }
+    listings = [
+        {'title': f'{query} for sale at Dealer A', 'price': '$28,000', 'link': 'https://dealera.com/listing1'},
+        {'title': f'{query} on EquipmentTrader', 'price': '$29,500', 'link': 'https://equipmenttrader.com/listing2'}
+    ]
+    return jsonify({'specs': specs, 'listings': listings})
+
+if __name__ == '__main__':
+    app.run(debug=True)
